3d6b88db9a7c82db5b4767fd417d057b2d1290ef
yog <yog@yogs-MacBook-Pro.local>
2023-08-19 19:54:10 +0530

    Multi select dropdown style changed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Select from 'react-select';
 import Header from './components/Header';
 import JsonData from './components/JsonData';
 const API = 'http://localhost:3002/movies/';
@@ -16,11 +17,22 @@ function getMonth(isoDate) {
 function App() {
   const [query, setQuery] = useState("");
 
+  const [moviesData, setMoviesData] = useState([]);
+
+  const [uniqueGenres, setUniqueGenres] = useState([]);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+
   const search = (data) => {
-    return data.filter((item) => item.title.toLowerCase().includes(query));
+    return data.filter(
+      (item) => item.title.toLowerCase().includes(query)
+        || item.genre.toLowerCase().includes(selectedGenres)
+    );
   };
 
-  const [moviesData, setMoviesData] = useState([]);
+  const options = uniqueGenres.map(genre => ({
+    value: genre,
+    label: genre,
+  }));
 
   useEffect(() => {
     fetch(API).then((result) => {
@@ -35,41 +47,84 @@ function App() {
           "released": new Date(movie.released).getUTCFullYear(),
           "rating": movie.imdb_rating,
           "runtime": movie.runtime,
-          // "genre": movie.genre
+          "genre": movie.genre
         })));
+
+        const genresSet = new Set();
+
+        data.forEach(item => {
+          item.genre.forEach(genre => {
+            genresSet.add(genre);
+          });
+        });
+
+        // console.log(genresSet);
+
+        setUniqueGenres(Array.from(genresSet));
 
         setMoviesData(data);
       })
     })
   }, []);
 
+  const handleGenreChange = selectedOptions => {
+    console.log('selectedOptions', selectedOptions);
+    setSelectedGenres(selectedOptions);
+  };
+
   return (
     <div className="container-md">
 
-      <Header />
+      <div className="row">
+        <div className="col">
+          <Header />
+        </div>
+      </div>
 
       <div className="row">
         <div className="col">
-          <select className="form-select list-group-item-dark"
+          {/* <select className="form-select list-group-item-dark"
             multiple aria-label="multiple select example">
             <option defaultValue={0}>Select Genre</option>
             <option value="1">One</option>
             <option value="2">Two</option>
             <option value="3">Three</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-          </select>
+          </select> */}
+          <Select styles={{
+            control: (baseStyles, state) => ({
+              ...baseStyles,
+              backgroundColor: '#212529',
+            }),
+            menu: (baseStyles) => ({
+              ...baseStyles,
+              backgroundColor: '#212529',
+              color: '#778088'
+            }),
+            multiValue: (baseStyles) => ({
+              ...baseStyles,
+              backgroundColor: '#9ba2a9',
+            })
+          }}
+            isMulti className='dark' placeholder='Select Genre'
+            options={options} value={selectedGenres} onChange={handleGenreChange}
+          />
+          <br />
+          {/* <p>Selected Genres: {selectedGenres.map(genre => genre.value).join(', ')}</p> */}
         </div>
         <div className="col">
-          <label className="form-label text-light">Search Movie</label>
-          <input type="text" className="form-control bg-dark text-light"
+          {/* <label className="form-label text-light">Search Movie</label> */}
+          <input type="text" className="form-control bg-dark text-light" placeholder='Search Movie'
             id="searchText" onChange={(event) => setQuery(event.target.value)}
           />
         </div>
       </div>
 
-      <JsonData moviesData={search(moviesData)} />
+      <div className="row">
+        <div className="col">
+          <JsonData moviesData={search(moviesData)} />
+        </div>
+      </div>
+
     </div>
   );
 }
